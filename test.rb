b02ea471d81dd52550670c311c55ee42b4b89b96
jeanarm <nizigiarmel2015@gmail.com>
2019-07-31 16:19:56 +0200

    second commit

--- test.rb
+++ test.rb
@@ -1,4 +1,3 @@
-
 require 'webrick'
 
 module WEBrick
@@ -6,22 +5,18 @@ module WEBrick
             FileHandler.add_handler('rb', CGIHandler)
           end
         end
+
 server = WEBrick::HTTPServer.new({
   :DocumentRoot => '.',
           :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
-          :Port => '3002',
+          :Port => '3000',
 })
 ['INT', 'TERM'].each {|signal|
           Signal.trap(signal){ server.shutdown }
         }
 
-# Change WEBrick :: HTTPServlet :: FileHandler to WEBrick :: HTTPServlet :: ERBHandler
-# Change 'test.html' to 'test.html.erb'
-server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
-
-server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
-
-# Add this line
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya1.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya1.rb')
 
 server.start
